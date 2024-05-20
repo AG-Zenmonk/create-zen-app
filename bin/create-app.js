3630c42e8188be9397b769136390f3f138c81afa
Aditya Goyal <aditya.1155@zenmonk.tech>
2024-05-20 18:36:31 +0530

    new branch with bin /config only

--- bin/create-app.js
+++ bin/create-app.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-
-// rest of the code
+const readline = require('readline');
 const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
+//check for dir name argv
 if (process.argv.length < 3) {
     console.log('You have to provide a name to your app.');
     console.log('For example :');
@@ -15,7 +15,8 @@ const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
 const git_repo = "https://github.com/AG-Zenmonk/create-zen-app.git";
-
+const yes = new RegExp("yes","i")
+//check for available directory
 try {
     fs.mkdirSync(projectPath);
 } catch (err) {
@@ -26,6 +27,13 @@ try {
     }
     process.exit(1);
 }
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
 async function main() {
     try {
         console.log('Downloading files...');
@@ -38,7 +46,7 @@ async function main() {
 
         console.log('Removing useless files');
         execSync('npx rimraf ./.git');
-        fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true });
+        fs.rmSync(path.join(projectPath, 'bin'), { recursive: true });
 
         console.log('The installation is done, this is ready to use !');
 
@@ -46,4 +54,20 @@ async function main() {
         console.log(error);
     }
 }
-main();
+
+rl.question('Which directory do you want to install Zen-ui-next-tempelete(Yes/No) ? ', (directory) => {
+
+    try {
+        if (yes.test(directory)) {
+            main()
+            console.log('===Zen-ui-next-tempelete installed successfully===');
+        }
+        else {
+            console.log('===Operation Terminated successfully===');
+            fs.rmdirSync(projectPath);
+        }
+    } catch (error) {
+        console.log('Error executing command:', error);
+    }
+    rl.close();
+});
